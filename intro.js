01cdff1738e64913158fa1eba80d9dee98c18e67
Ethan <ez4698@gmail.com>
2023-02-28 15:41:48 -0700

    added demo file

---
+++ intro.js
@@ -0,0 +1,32 @@
+//console.log("Hello, world")
+
+const firstName = "Ethan"
+
+//console.log(firstName)
+
+let age = (23)
+
+const isCool = true
+
+//console.log(firstName, age, isCool)
+
+//console.log("Hello, my name is" + firstName +" I am " + age +" years old.")
+
+let x = 4
+let y = 10
+let z = x + 8
+
+let sum = x + y+ z
+
+// console.log(x)
+// console.log(y)
+//console.log(z)
+
+//console.log(sum)
+
+if (x===y){
+    console.log('X and Y are the same value')
+} else {
+    console.log('X and y are different values')
+}
+
